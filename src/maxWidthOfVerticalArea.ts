34efec031baa440c6fe8c323bc34634ef6998aaf
arup upopadhyay <arup.upopadhyay@yahoo.com>
2024-03-14 16:37:23 +0530

    Added maxWidthOfVerticalArea Problem

--- src/maxWidthOfVerticalArea.ts
+++ src/maxWidthOfVerticalArea.ts
@@ -1,6 +1,6 @@
 function maxWidthOfVerticalArea(points: number[][]): number {
-    let max = 0;
-    const xAxisIsSorted = points.map(point => point[0]).sort((a,b) => a - b);
+    let max : number = 0;
+    const xAxisIsSorted: number[] = points.map(point => point[0]).sort((a,b) => a - b);
     for(let pos = 1; pos< xAxisIsSorted.length; pos++) {
         max = Math.max(max, xAxisIsSorted[pos] - xAxisIsSorted[pos - 1]);
     }
